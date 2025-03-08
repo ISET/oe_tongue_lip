b4e1942a97ce2dadc3468a28a45418ce0a37ca65
Brian Wandell <wandell@stanford.edu>
2025-03-07 16:59:27 -0800

    Simplified and made work for 'or' condition

--- tablebase/ieTableGet.m
+++ tablebase/ieTableGet.m
@@ -1,51 +1,63 @@
-function files = ieTableGet(T,varargin)
-% Return the file names that match the conditions in varargin
-%
-% To do:  Maybe we make 'file' or some other variable as an optional
-%   return.  Otherwise, we could return the subset of table rows (default).
-%   Use this a lot:  rows = find(T.(variableNames{ii}) == val);
-%   and also intersect(rows1,rows2).
+function tableReturn = ieTableGet(T,varargin)
+% Return the rows that match the conditions in varargin
 %
 % Synopsis
-%   files = ieTableGet(T,varargin)
+%   tableReturn = ieTableGet(T,varargin)
 %
 % Brief
-%   We manage data using tables with variables, in some cases.  The
-%   tables include a slot for the data file.  We use this interface to
-%   specify which rows of the table have the file of interest.
+%   We sometimes manage data using tables with variables.  We use this
+%   function to retrieve rows of the table whose fields match the specified
+%   variables.
 %
 % Inputs
 %   T - A table 
 %
 % Optional key/val pairs
 %   operator - Either 'and' or 'or'.  Default: 'and'
-%   fields - The table field names are permissible.  These strings should
-%            be lower case in this ISETCam environment.
+%
+%   fields - The table field names with a specified condition are all
+%            permissible for the field. The field strings should be lower
+%            case in this ISETCam environment.
+%
+%   return - Type of data to be returned.  By defult, it is the rows of the
+%            table ('table rows').  It can be the data files (T.file)
+%            assocated with the rows. 
 %
 % Return
-%   files - String array of file names matching the condition(s)
+%   tableReturn - String array of file names matching the condition(s)
+%
+% Description:
+%
+%   The data table, T, has metadata fields and usually a slot for the data
+%   file. We use this function, ieTableGet, to retrieve the subset of table
+%   rows whose fields match the values specified in the input arguments. We
+%   can also ask ieTableGet to return an array of strings with the file
+%   names that match.
+%
+%   The varargin fields beyond 'operator' and 'return' must be a table
+%   field name and value.  The same field name can be used multiple times
+%   when the operation is an 'or'.  For an 'and' operation, multiple field
+%   names is likely to lead to an empty return.
 %
 % See also
 %   table
 
 % Example:
 %{
+% 'and' case
 T = oeDatabaseCreate;
-variableTypes = varfun(@class, T, 'OutputFormat', 'cell')
-
-files = ieTableGet(T,'ewave',415)
-variableTypes = varfun(@class, T, 'OutputFormat', 'cell')
-
-files = ieTableGet(T,'subject','J')
-variableTypes = varfun(@class, T, 'OutputFormat', 'cell')
-
-files = ieTableGet(T,'ewave',415,'subject','J','substrate','tongue')
-variableTypes = varfun(@class, T, 'OutputFormat', 'cell')
-
-% Only subjects Z and J
-files = ieTableGet(T,'operator','or','subject','Z','subject','J');
-variableTypes = varfun(@class, T, 'OutputFormat', 'cell')
-
+tRows = ieTableGet(T,'ewave',415); size(tRows,1)
+tRows = ieTableGet(T,'subject','J'); size(tRows,1)
+tRows = ieTableGet(T,'ewave',415,'subject','J','substrate','tongue'); size(tRows,1)
+files = ieTableGet(T,'ewave',415,'subject','J','substrate','tongue','return','file')
+%}
+%{
+% 'or' case
+T = oeDatabaseCreate;
+tRows = ieTableGet(T,'subject','Z');size(tRows,1)
+tRows = ieTableGet(T,'subject','J');size(tRows,1)
+tRows = ieTableGet(T,'operator','or','subject','Z','subject','J');size(tRows,1)
+files = ieTableGet(T,'operator','or','subject','Z','subject','J','return','files');size(files,1)
 %}
 
 %% Parse
@@ -57,87 +69,61 @@ variableNames = T.Properties.VariableNames;
 variableTypes = varfun(@class, T, 'OutputFormat', 'cell');
 
 p = inputParser;
+p.KeepUnmatched = true;
+
 p.addRequired('T',@istable);
 p.addParameter('operator','and',@(x)(ismember(x,{'and','or'})));
-
-% We have to loop over the varargin vals, not the variable Names.  If
-% the varargin entry is a variable name, then process it.
-passedV = zeros(size(variableNames));
-for vv=1:numel(variableNames)
-    thisV = variableNames{vv};
-    thisT = variableTypes{vv};
-    
-    % Doesn't deal with the case of two args for the same field, say
-    % subject Z and subject D.  
-    if any(strcmp(thisV, varargin))
-        passedV(vv) = 1;
-        % User passed in a condition for this variable
-        if strcmp(thisT,'string')            
-            p.addParameter(thisV,'',@ischar);
-        elseif strcmp(thisT,'double')            
-            p.addParameter(thisV,[],@isnumeric);
-        else
-            error('Variable types must be string or double. This is %s',thisT);
-        end
-    end
-end
+p.addParameter('return','rows',@ischar);
 
 p.parse(T,varargin{:});
 op = p.Results.operator;
-idx = find(passedV);
-rows = '';
-switch op
-    case 'and'
-        for ii=idx
-            switch variableTypes{ii}
-                case 'string'
-                    val = p.Results.(variableNames{ii});
-                    % tmp = T(strcmp(T.(variableNames{ii}),val),:).file; 
-                    tmp = find(T.(variableNames{ii}) == val);
-                case 'double'
-                    val = p.Results.(variableNames{ii});
-                    % tmp = T(T.(variableNames{ii}) == val,:).file;
-                    tmp = find(T.(variableNames{ii}) == val);
-            end
+tReturn = p.Results.return;
 
-            if isempty(rows), rows = tmp;
-            else,             rows = intersect(rows,tmp);
-            end
+%% Walk through conditions in varargin
+
+% If the varargin entry is a variable name, then process it.
+
+rows = [];
+for ii=1:2:numel(varargin)
+    if ismember(varargin{ii},{'operator','return'})
+        % do nothing
+    else
+        % Find the argument and its type.
+        [~,idx] = ismember(varargin{ii},variableNames);
+        if isempty(idx), error('%d: %s is not a variable name.',ii,varargin{ii});
+        else, thisV = variableNames{idx}; thisT = variableTypes{idx}; val = varargin{ii+1}; end
+
+        switch thisT
+            case 'string'
+                tmp = find(T.(thisV) == val);
+            case 'double'
+                tmp = find(T.(thisV) == val);
         end
 
-    case 'or'
-        disp('NY');
-        return;
-        %{
-        for ii=idx
-            switch variableTypes{ii}
-                case 'string'
-                    val = p.Results.(variableNames{ii});
-                    tmp = T(strcmp(T.(variableNames{ii}),val),:).file;                    
-                case 'double'
-                    val = p.Results.(variableNames{ii});
-                    tmp = T(T.(variableNames{ii}) == val,:).file;
-            end
-            if isempty(files), files = tmp;
-            else,              files = cat(1,files,tmp);
-            end
-        end
-        files = unique(files);
-        %}
-    otherwise
-        error('Unknown operator %s\n',op);
-end
+        switch op
+            case 'and'
+                if isempty(rows), rows = tmp;
+                else,             rows = intersect(rows,tmp);
+                end
 
-if iscell(files), files = string(files)'; end
+            case 'or'
+                if isempty(rows), rows = tmp;
+                else,             rows = unique(cat(1,rows,tmp));
+                end
 
-end
-
-%% ----
-function commonStrings = findCommonStrings(files, tmp)
-    commonStrings = {};
-    for ii = 1:length(files)
-        if ismember(files{ii}, tmp)
-            commonStrings{end+1} = files{ii}; %#ok<AGROW>
+            otherwise
+                error('Unknown operator %s\n',op);
         end
     end
+end
+
+switch tReturn
+    case 'rows'
+        tableReturn = T(rows,:);
+    case {'files','file'}
+        tableReturn = T(rows,:).file;
+    otherwise
+        error('Unknown return type %s\n',tReturn);
+end
+
 end
