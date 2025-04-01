6f5fc90de87874ca42e3b4b70ae0e6febc02c409
Brian Wandell <wandell@stanford.edu>
2025-03-31 20:16:07 -0700

    Made a search for 2 dimensions on the lip

--- utilities/oeSolveFluorophoresSkewedGaussiansLip.m
+++ utilities/oeSolveFluorophoresSkewedGaussiansLip.m
@@ -1,4 +1,4 @@
-function [global_params, weights, Gaussians] = oeSolveFluorophoresSkewedGaussians(wave, data, oxyTransmittance)
+function [global_params, weights, Gaussians] = oeSolveFluorophoresSkewedGaussiansLip(wave, data, oxyTransmittance)
 % oeSolveFluorophoresSkewedGaussians - Fits a weighted sum of three skewed
 % Gaussians to multiple spectra. 
 % 
@@ -23,9 +23,9 @@ function [global_params, weights, Gaussians] = oeSolveFluorophoresSkewedGaussian
 %  Enable setting how many skewed Gaussians to create.  From 2 to maybe 4
 %  would be a good range.
 %
-% This code is designed to find 3 fluorophores for the tongue.  The code is
-% not general.  If we want to just find 2 fluorophores for the lip, we need
-% a separate version.
+% This code is designed to find 2 fluorophores for the lip.  The code is
+% not general.  If we want to find 3 fluorophores for the tongue, we need
+% use the other version.
 %
 % See also
 %
@@ -33,31 +33,6 @@ function [global_params, weights, Gaussians] = oeSolveFluorophoresSkewedGaussian
 
 % Example:
 %{
-
-wave = linspace(400, 700, 100); % Example x-values (e.g., wavelengths)
-data = rand(100, 10); % Replace with actual spectral data (100 points, 5 spectra)
-
-oxyblood = medium('oxy_molarExtinctionCoefficient.mat','wave',wave);
-oxyblood.opticalDensity = 2;
-oxyTransmittance = oxyblood.transmittance;
-% plot(wave,oxyTransmittance);
-
-[global_params, weights, Gaussians] = oeSolveFluorophoresSkewedGaussians(wave, data, oxyTransmittance);
-
-disp('Optimized Gaussian Parameters:');
-disp(global_params);
-
-disp('Non-Negative Weights for each spectrum:');
-disp(weights);
-
-% This is the error. 
-fitError = data - Gaussians*weights;
-rmserror = norm(fitError(:),2);
-disp('RMSE');
-disp(rmserror);
-
-ieFigure; plot(wave,Gaussians);
-
 %}
 
 assert(numel(oxyTransmittance) - size(data,1) == 0);
@@ -69,19 +44,16 @@ num_spectra = size(data, 2);  % Number of spectra
 % separate version.
 
 % Initial guesses for shared parameters.
-% Collagen, keratin, FAD
-mu0 = [430 475 530];
-% mu0    = linspace(min(wave), max(wave), 3);  % Evenly spaced initial guesses for mu
-sigma0 = std(wave) * [0.5, 1, 1.5];  % Different scales for sigma
-a0     = [0, 0, 0];  % Initial skewness
+mu0 = [400 530];      % Collagen and FAD
+sigma0 = [100, 100];  % Spread
+a0     = [0, 0];      % Initial skewness
 
-% List of 9 parameters
+% List of 6 parameters
 p0 = [mu0, sigma0, a0];
 
 % Lower and upper bounds for shared parameters
-% We set the peaks to be near collagen (), Keratin
-lb = [350, 400, 450, 10, 10, 10, -15, -15, -15];
-ub = [450  550  550, 200, 200, 200, 15, 15, 15];
+lb = [390, 460, 10, 10,  -25, -25];
+ub = [420  560, 300, 300, 25, 25];
 
 % We will change this to incorporate blood.
     function Gaussians = skewed_gaussians(p, x)
@@ -89,9 +61,8 @@ ub = [450  550  550, 200, 200, 200, 15, 15, 15];
             (normpdf((x - mu) / sigma) .* (1 + normcdf(a * x)))'; % Transpose to ensure column output
 
         % Ensure Gaussians is a matrix with columns as separate functions
-        Gaussians = [skewed_gaussian(p(1), p(4), p(7), x), ...
-            skewed_gaussian(p(2), p(5), p(8), x), ...
-            skewed_gaussian(p(3), p(6), p(9), x)];
+        Gaussians = [skewed_gaussian(p(1), p(3), p(5), x), ...
+            skewed_gaussian(p(2), p(4), p(6), x)];
     end
 
 % Objective function for optimizing the shared parameters
@@ -102,7 +73,7 @@ ub = [450  550  550, 200, 200, 200, 15, 15, 15];
         Gaussians(:,1) = Gaussians(:,1).*oxyTransmittance;
 
         % Now solve for the weights
-        weights = zeros(3, num_spectra);
+        weights = zeros(2, num_spectra);
 
         % Solve for non-negative weights using lsqnonneg for each spectrum
         for j = 1:num_spectra
@@ -130,7 +101,7 @@ global_params = lsqnonlin(@fit_global_params, p0, lb, ub, options);
 Gaussians = skewed_gaussians(global_params, wave);
 Gaussians(:,1) = Gaussians(:,1).*oxyTransmittance;
 
-weights = zeros(3, num_spectra);
+weights = zeros(2, num_spectra);
 
 for j = 1:num_spectra
     weights(:, j) = lsqnonneg(Gaussians, data(:, j));
